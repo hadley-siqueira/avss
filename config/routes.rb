f15d9685c6b5e24ecaa182f87296d315cc497f96
Hadley Siqueira <hadley.siqueira@gmail.com>
2022-12-07 14:39:21 -0300

    working on station cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :stations do
+    resource :title, module: :stations
   end
 
-  get 'stations/:id/title', to: 'stations/title#show', as: 'foobar'
-  patch 'stations/:id/title', to: 'stations/title#update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
